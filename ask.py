a9396174ce820bad7ddd1e3b67511ee3d3b6f1e8
atharvaajmera <atharvaajmera06@gmail.com>
2026-01-10 20:22:54 +0530

    implemented 2 phase retrieval

--- ask.py
+++ ask.py
@@ -1,6 +1,4 @@
-from memory import query_text
-
-threshold= 1.2
+from memory import query_text_phase_2
 
 if __name__ == "__main__":
     print("Enter your query:")
@@ -11,7 +9,7 @@ if __name__ == "__main__":
             print("Exiting...")
             break
 
-        res=query_text(user_query)
+        res=query_text_phase_2(user_query)
 
         if not res:
             print("No relevant messages found.")
@@ -20,8 +18,6 @@ if __name__ == "__main__":
         for item in res:
             meta=item['metadata']
             text=item['document']
-            confidence=item['distance']
-            if confidence <= threshold:
-                 print(f"User ({meta['user']}) at {meta['ts']}: said {text} with a confidence score of {confidence}\n")
-            else:
-                 print(f"Skipping message from User ({meta['user']}) at {meta['ts']} due to low confidence score of {confidence}\n")
+            # confidence=item['distance']
+            print(f"User ({meta['user']}) at {meta['ts']}: said {text}")
+            
--- memory.py
+++ memory.py
@@ -10,10 +10,11 @@ def add_messages(texts, ids, metadatas):
     collection.upsert(documents=texts, ids=ids, metadatas=metadatas)
     print("Got the threads from ingest and added to memory.")
 
-def query_text(query):
+
+def query_text_phase_1(query):
     results = collection.query(
         query_texts=[query],
-        n_results=5
+        n_results=1
     )
 
     if not results['documents']:
@@ -26,11 +27,42 @@ def query_text(query):
     outputs = []
    
     for doc,metadata,id,distance in zip(documents,metadatas,ids,distances):
+        if distance < 1.2:
+            output = {
+                "id": id,
+                "document": doc,
+                "metadata": metadata,
+                "distance": distance
+            }
+            outputs.append(output)
+    return outputs
+
+def get_top_convo_id(query):
+    anchor=query_text_phase_1(query)
+    top_conversation_id=anchor[0]['metadata']['thread_id'] if anchor else None
+    return top_conversation_id
+
+def query_text_phase_2(query):
+    top_convo_id=get_top_convo_id(query)
+    if not top_convo_id:
+        return []
+    results = collection.get(
+        where={
+            "thread_id": top_convo_id
+        }
+    )
+    if not results['documents']:
+        return []
+    ids = results['ids']
+    documents = results['documents']
+    metadatas = results['metadatas']
+    outputs = []
+    for doc,metadata,id in zip(documents,metadatas,ids):
         output = {
             "id": id,
             "document": doc,
-            "metadata": metadata,
-            "distance": distance
+            "metadata": metadata
         }
         outputs.append(output)
+    outputs.sort(key=lambda x: float(x['metadata']['ts']))
     return outputs
